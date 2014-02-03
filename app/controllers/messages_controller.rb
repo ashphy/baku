da55b52a3c5be5f7f0c426af1f4be5dbbef7d83b
Kazuki Hamasaki <ne.vivam.si.abis@gmail.com>
2014-02-03 23:22:13 +0900

    チャンネルと日付で絞り込む機能を追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,12 +4,12 @@ class MessagesController < ApplicationController
   # GET /messages
   # GET /messages.json
   def index
-    @messages = Message.all
     # 検索フォームの入力内容で検索する
     @q = Message.search(params[:q])
 
     # 重複を排除
     @messages = @q.result(distinct: true)
+    @channels = Channel.all
   end
 
   # GET /messages/1
