10d608efd0e3eb7b828583be64b3bd401b835557
Kazuki Hamasaki <ne.vivam.si.abis@gmail.com>
2014-02-11 15:03:19 +0900

    検索フォームはデフォルトで全表示しないようにした

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,19 +2,17 @@ class SearchController < ApplicationController
   # GET /search
   # GET /search.json
   def index
-    @q = Message.search(params[:q])
-    @messages = @q.result(distinct: true)
-    @channels = Channel.all
+    if params[:q].present?
+      @q = params[:q]
+      @search = Message.search(text_cont: @q)
+      @messages = @search.result(distinct: true)
+      @channels = Channel.all
+    end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_message
       @message = Message.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def message_params
-      params.require(:message).permit(:text, :user, :command)
     end
 end
