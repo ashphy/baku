4b36d18059e6dd641342210c8e96eb2de8c45f07
Kazuki Hamasaki <ne.vivam.si.abis@gmail.com>
2014-02-16 22:10:57 +0900

    Messageのvalidationを追加

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,13 @@
 class Message < ActiveRecord::Base
   belongs_to :channel
+
+  validates_presence_of :text
+  validates_length_of :text, maximum: 512
+
+  validates_presence_of :user
+  validates_length_of :user, maximum: 20
+
+  validates_presence_of :command
+  validates :command, :inclusion => ['PRIVMSG', 'NOTICE']
+
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,51 @@
 require 'spec_helper'
 
 describe Message do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) { 'test' }
+  let(:text) { 'text' }
+  let(:command) { 'PRIVMSG' }
+  let(:message) { Message.new(user: user, text: text, command: command) }
+
+  it 'PRIVMSG should save' do
+    message.save.should be_true
+    message.user.should == user
+    message.text.should == text
+    message.command.should == command
+  end
+
+  it 'NOTICE should save' do
+    message.command = 'NOTICE'
+    message.save.should be_true
+    message.command.should == 'NOTICE'
+  end
+
+  it 'invalid command should not save' do
+    message.command = 'hoge'
+    message.save.should be_false
+  end
+
+  it 'empty user should not save' do
+    message.user = nil
+    message.save.should be_false
+    message.user = ''
+    message.save.should be_false
+  end
+
+  it 'empty user should not save' do
+    message.text = nil
+    message.save.should be_false
+    message.text = ''
+    message.save.should be_false
+  end
+
+  it 'long message should save' do
+    message.text = 'a' * 512
+    message.save.should be_true
+  end
+
+  it 'longest message should save' do
+    message.text = 'a' * 513
+    message.save.should be_false
+  end
 end
