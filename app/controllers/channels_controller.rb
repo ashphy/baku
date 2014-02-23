9959099137ac424e843500f3770f3ea352a308ac
Kazuki Hamasaki <ne.vivam.si.abis@gmail.com>
2014-02-23 15:52:41 +0900

    Revert channel controller for admin to view the list of channels

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,29 +1,11 @@
 class ChannelsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!
   before_action :set_channel, only: [:show, :edit, :update, :destroy]
 
   # GET /channels
   # GET /channels.json
   def index
     @channels = Channel.all.order(:name)
-
-    if params[:id].present?
-      @channel = @channels.get_channel("##{params[:id]}")
-    end
-
-    if params[:year].present?
-      @year = params[:year]
-    end
-
-    if params[:month].present?
-      @month = params[:month]
-    end
-
-    if params[:day].present?
-      @day = params[:day]
-      @date = Date.new(@year.to_i, @month.to_i, @day.to_i)
-      @messages = Message.daily_log(@channel, @date)
-    end
   end
 
   # GET /channels/1
