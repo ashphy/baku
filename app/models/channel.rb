6f79f93d90c483c3e5998959fa8f1c03d56470b7
Kazuki Hamasaki <ne.vivam.si.abis@gmail.com>
2016-03-07 22:36:48 +0900

    Fixed to calculate a date range taking into account the time zone

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -33,7 +33,7 @@ class Channel < ActiveRecord::Base
   end
 
   def months(year)
-    date = Date.new(year.to_i, 1, 1)
+    date = Time.zone.local(year.to_i, 1, 1)
     range = date.beginning_of_year..date.end_of_year
     logged_months = Message.where(channel_id: id).group_by_month(:created_at, range: range).count.select { |date, count| count > 0 }
     logged_months.map do |date, count|
@@ -42,7 +42,7 @@ class Channel < ActiveRecord::Base
   end
 
   def days(year, month)
-    date = Date.new(year.to_i, month.to_i, 1)
+    date = Time.zone.local(year.to_i, month.to_i, 1)
     range = date.beginning_of_month..date.end_of_month
     logged_days = Message.where(channel_id: id).group_by_day(:created_at, range: range).count.select { |date, count| count > 0 }
     logged_days.map do |date, count|
